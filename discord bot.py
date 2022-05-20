64fc2f818af352591ca2d3bd8e0f63b27261aa07
epicbuilder2007 <elonlou@gmail.com>
2022-05-20 23:41:32 +0800

    added :suslist and updated :Help, bumped version to 1.8

--- discord bot.py
+++ discord bot.py
@@ -7,9 +7,10 @@ from discord.ext import commands
 import json
 from numpy import ndarray
 
-Version = 1.6
+Version = 1.8
 
 Galaxylist = pandas.read_excel('Galaxydps.xlsx', index_col = 0)
+
 
 async def writeJSON(ctx, ship, arg):
     JSON = open(f'{ctx.author.id}.json', "r")
@@ -22,6 +23,8 @@ async def writeJSON(ctx, ship, arg):
     pref = json.dumps(pref, indent = 4)
     JSON.write(pref)
     JSON.close()
+
+
 async def concTurret(ctx, ship):
     string = ""
     for i in range(2):
@@ -33,6 +36,8 @@ async def concTurret(ctx, ship):
             string = string + f'Turret {i+1}: ' + str(Galaxylist[f'Turret{i+1}_type'][str(ship)]) + " x" + str(int(Galaxylist[f'Turret{i+1}_amount'][str(ship)])) + "\n"
 
     return str(string)
+
+
 async def outputResult(ctx, ship, mode):
     try:
         if mode == "info":
@@ -64,6 +69,8 @@ async def outputResult(ctx, ship, mode):
                 "Maximum Range: " + str(Galaxylist['maxRange'][str(ship)]))
     except:
         await ctx.send("Please check if you included mode argument. the command is :ship <ship> <info/turret>")
+
+
 async def newJSON(ctx):
     with open(f'{ctx.author.id}.json', "a+") as JSON:
         pref = {
@@ -78,11 +85,47 @@ async def newJSON(ctx):
         JSON.close()
 
 
+async def shipCommand(ctx, arg, mode):
+    global JSON
+    try:
+        JSON = open(f'{ctx.author.id}.json', "r")
+        pref = json.load(JSON)
+    except FileNotFoundError:
+        await newJSON(ctx)
+        JSON = open(f'{ctx.author.id}.json', "r")
+        pref = json.load(JSON)
+
+    if arg in pref['set_auto_referral']:
+        await outputResult(ctx, pref['set_auto_referral'][arg], mode)
+        await writeJSON(ctx, pref['set_auto_referral'][arg], arg)
+
+    elif arg == pref['last_search_string']:
+        await outputResult(ctx, pref['last_search_return_ship'], mode)
+        await writeJSON(ctx, pref['last_search_return_ship'], arg)
+    else:
+        findlist = []
+        found = False
+        for key, value in Galaxylist['Type'].items():
+            if str(key).find(arg.title()) >= 0:
+                findlist.append((str(key), str(value)))
+        for i in range(len(findlist)):
+            if findlist[i][1] == pref['last_search_ship_type']:
+                await outputResult(ctx, findlist[i][0], mode)
+                await writeJSON(ctx, findlist[i][0], arg)
+                found = True
+                break
+
+        if not found:
+            await outputResult(ctx, findlist[0][0], mode)
+            await writeJSON(ctx, findlist[0][0], arg)
+
+
 load_dotenv()
 TOKEN = "NzEzMDE2NzY3OTc0NDczNzU5.XsZ-nA.TvQsvuJepWTcKRNO5KkzIXTal3Q"
 bot = commands.Bot(command_prefix=":")
 
-#replace this with :range, :info, and :turret
+"""
+replace this with :range, :info, and :turret
 @bot.command(name='ship')
 async def dpsCheck(ctx, arg, mode="info"):
     global JSON
@@ -132,7 +175,7 @@ async def dpsCheck(ctx, arg, mode="info"):
                 break
         if not found:
             await ctx.send(f"Search for {arg} failed :(")
-
+"""
 
 @bot.command(name='short')
 async def func(ctx, short="", *, arg):
@@ -239,6 +282,8 @@ async def func(ctx, short="", *, arg):
     else:
         await ctx.send("Operation not found, please run :short help for help on the command.")
     """
+
+
 @bot.command(name="hitlist")
 async def hitlist(ctx, player = "", *, arg):
     args = arg.split()
@@ -315,68 +360,53 @@ async def hitlist(ctx, player = "", *, arg):
             except Exception as e:
                 await ctx.send(str(e))
     """
+
+
 @bot.command(name="range")
 async def Range(ctx, arg):
-    global JSON
-    try:
-        JSON = open(f'{ctx.author.id}.json', "r")
-        pref = json.load(JSON)
-    except:
-        await newJSON(ctx)
-        JSON = open(f'{ctx.author.id}.json', "r")
-        pref = json.load(JSON)
-
-    if arg in pref['set_auto_referral']:
-        await outputResult(ctx, pref['set_auto_referral'][arg], "range")
-        await writeJSON(ctx, pref['set_auto_referral'][arg], arg)
+    await shipCommand(ctx, arg, "range")
 
 
-    elif arg == pref['last_search_string']:
-        await outputResult(ctx, pref['last_search_return_ship'], "range")
-        await writeJSON(ctx, pref['last_search_return_ship'], arg)
+@bot.command(name="info")
+async def info(ctx, arg):
+    await shipCommand(ctx, arg, "info")
 
-    else:
-        findlist = []
-        found = False
-        for key, value in Galaxylist['Type'].items():
-            if str(key).find(arg.title()) >= 0:
-                findlist.append((str(key), str(value)))
-        for i in range(len(findlist)):
-            if findlist[i][1] == pref['last_search_ship_type']:
-                await outputResult(ctx, findlist[i][0], "range")
-                await writeJSON(ctx, findlist[i][0], arg)
-                found = True
-                break
 
-        if found == False:
-            await outputResult(ctx, findlist[0][0], "range")
-            await writeJSON(ctx, findlist[0][0], arg)
+@bot.command(name="turret")
+async def turret(ctx, arg):
+    await shipCommand(ctx, arg, "turret")
+
 
 @bot.command(name="Help")
 async def help(ctx):
     await ctx.send(f"This is version {Version} of Tau Gamma Bot, home brewed by epicbuilder2007, who definitely doesn't have other stuff to do. \n\n"+
-                   "There are currently 5 commands for this bot: \n\n"+
-                   "command :ship <string> <info/turret> \n"+
-                   "Use this command to see the info of a ship. More stuff's coming for this command, at least if I have enough time to set them in. \n"+
-                   "'info' gives basic information \n"+
-                   "'turret' gives the information of turrets and spinals on the ship \n\n"+
-                   "command :short <set/remove/print> <short> <ship (optional)> \n"+
+                   "There are currently  commands for this bot: \n\n"+
+                   "command :info <string> \n"+
+                   "This returns general info of the ship. \n\n"+
+                   "command :turret <string> \n"+
+                   "This returns turret info (dps and name) of the ship. \n\n"+
+                   "command :range <string> \n"+
+                   "This returns range info of the ship \n\n"+
+                   "command :short <short> <ship (optional)> <set/remove/print> \n"+
                    "Use this command to set shortcuts to ships.\n"+
                    "'set' sets the short name you provided to the ship name you provided. Remember that currently, you do need to make sure the ship name is case-correct. \n"+
                    "'remove' removes the short name you provided. \n"+
                    "'print' prints the short name you set before \n\n"+
-                   "command :hitlist <add/view/viewall> <player> <reason> \n"+
+                   "command :hitlist <player> <reason (optional)> <add/view/viewall> \n"+
                    "Use this command to keep tabs on which players the clan wants to kill. \n"+
                    "'add' adds the player to the list. \n"+
                    "'view' views the reason why the player was added to the hitlist. \n"+
                    "'viewall' views all the players and their reason for their presence in the hitlist \n\n"+
-                   "command :range <string> (As requested from CubicalGuySays, I wonder if he's really a cubical guy... \n"+
-                   "Use this command to view the range of a ship \n\n"+
+                   "command :suslist <player> <add/view> \n"+
+                   "Use this command to keep track of suspicious players to avoid."+
+                   "'add' adds the player name provided to the list. \n"+
+                   "'view' returns the list of players to be careful around. \n\n"+
                    "command :services <service> <True/False> \n"+
                    "Use this command to enable or disable bot features for yourself. \n"+
                    "Current configurable services include (case-sensitive): 'predictive_search' \n\n"+
                    "If you have any feature requests, or bug reports, feel free to contact me (epicbuilder2007#8204) about it, whether via pinging me in General Chat or by DMing me, or even via Whatsapp if you have it."+
                    "Don't waste your time trying to tell me how shit this bot is though, I know that :sad:")
+
 
 @bot.command(name='services')
 async def services(ctx, service, status = True):
@@ -431,6 +461,24 @@ async def data(ctx, msg: discord.Message , ship = "", cat = "", string = ""):
         if string == "":
             await ctx.send("Please specify data value")
 
+
+@bot.command(name="suslist")
+async def sus(ctx, player = "", mode = "add"):
+    JSON = open('suslist.json', 'r')
+    pref = json.load(JSON)
+    JSON.close()
+    if mode.lower() == "add":
+        pref[player] = "obligatory value here"
+        JSON = open('suslist.json', 'w')
+        pref = json.dumps(pref, indent=4)
+        JSON.write(pref)
+        JSON.close()
+    elif mode.lower() == "view":
+        string = []
+        for key in pref.items():
+            string.append(key)
+        await ctx.send(f'Players in this list: {", ".join(string)}. Please be careful when around these players, as \
+        they are known for ruining the fun.')
 
 
 bot.run(TOKEN)
