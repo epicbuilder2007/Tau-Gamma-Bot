0ba866868e395d5ec1820e610c563f7cd584087c
epicbuilder2007 <elonlou@gmail.com>
2022-05-25 20:23:08 +0800

    test data code, changed prefix to !

--- discord bot.py
+++ discord bot.py
@@ -5,6 +5,7 @@ import pandas
 from pandas import *
 from discord.ext import commands
 import json
+import threading
 from numpy import ndarray
 
 Version = 1.8
@@ -65,6 +66,7 @@ async def outputResult(ctx, ship, mode):
                 )
         elif mode == "range":
             await ctx.send(
+                "Ship: " + str(ship) + "\n"
                 "Minimum Range: " + str(Galaxylist['minRange'][str(ship)]) +"\n"
                 "Maximum Range: " + str(Galaxylist['maxRange'][str(ship)]))
     except:
@@ -122,60 +124,8 @@ async def shipCommand(ctx, arg, mode):
 
 load_dotenv()
 TOKEN = ""
-bot = commands.Bot(command_prefix=":")
+bot = commands.Bot(command_prefix="!")
 
-"""
-replace this with :range, :info, and :turret
-@bot.command(name='ship')
-async def dpsCheck(ctx, arg, mode="info"):
-    global JSON
-    try:
-        JSON = open(f'{ctx.author.id}.json', "r+")
-        pref = json.load(JSON)
-    except:
-        await newJSON(ctx)
-        JSON = open(f'{ctx.author.id}.json', "r+")
-        pref = json.load(JSON)
-
-    if pref['enable_predictive_search'] == True:
-        if arg in pref['set_auto_referral']:
-            await outputResult(ctx, pref['set_auto_referral'][arg], mode)
-            await writeJSON(ctx, pref['set_auto_referral'][arg], arg)
-
-
-        elif arg == pref['last_search_string']:
-            await outputResult(ctx, pref['last_search_return_ship'], mode)
-            await writeJSON(ctx, pref['last_search_return_ship'], arg)
-
-        else:
-            findlist = []
-            found = False
-            for key, value in Galaxylist['Type'].items():
-                if str(key).find(arg.title()) >= 0:
-                    findlist.append((str(key), str(value)))
-            for i in range(len(findlist)):
-                if findlist[i][1] == pref['last_search_ship_type']:
-                    await outputResult(ctx, findlist[i][0], mode)
-                    await writeJSON(ctx, findlist[i][0], arg)
-                    found = True
-                    break
-
-            if found == False:
-                await outputResult(ctx, findlist[0][0], mode)
-                await writeJSON(ctx, findlist[0][0], arg)
-
-    else:
-        await ctx.send("Predictive search is off! Search results may not be accurate for you.")
-        found = False
-        for key, value in Galaxylist['Type'].items():
-            if str(key).find(arg.title()) >= 0:
-                await outputResult(ctx, str(key), mode)
-                await writeJSON(ctx, str(key), arg)
-                found = True
-                break
-        if not found:
-            await ctx.send(f"Search for {arg} failed :(")
-"""
 
 @bot.command(name='short')
 async def func(ctx, short="", *, arg):
@@ -187,7 +137,7 @@ async def func(ctx, short="", *, arg):
         elif "remove" in args:
             mode = "remove"
         elif "print" in args:
-            mode = "remove"
+            mode = "print"
     args[args.index(mode)].remove()
     global JSON
     try:
@@ -220,68 +170,6 @@ async def func(ctx, short="", *, arg):
             await ctx.send(f'{short} was set to ship {pref["set_auto_referral"][str(short)]}.')
         except KeyError:
             await ctx.send(f'Short {short} not found, did you make a typo?')
-
-
-
-
-
-
-    """
-    if mode == "":
-        await ctx.send("Please specify operation. Command is :short <set/remove/print/help>. Use :short help for help on command")
-
-    #set subcommand
-    elif mode.lower() == "set":
-        if short != "" and ship != "":
-            global JSON
-            try:
-                JSON = open(f'{ctx.author.id}.json', "r")
-                pref = json.load(JSON)
-            except:
-                await newJSON(ctx)
-                JSON = open(f'{ctx.author.id}.json', "r")
-                pref = json.load(JSON)
-            JSON.close()
-            JSON = open(f'{ctx.author.id}.json', "w")
-            pref["set_auto_referral"][str(short)] = ship
-            pref = json.dumps(pref, indent = 4)
-            JSON.write(pref)
-            JSON.close()
-            await ctx.send(f"Successfully mapped {short} to {ship}")
-        elif ship == "":
-            await ctx.send("specify ship name")
-
-        elif short == "":
-            await ctx.send("specify short name")
-
-    #remove subcommand
-    elif mode.lower() == "remove":
-        if short != "":
-            JSON = open(f'{ctx.author.id}.json', "r")
-            pref = json.load(JSON)
-            JSON.close()
-            JSON = open(f'{ctx.author.id}.json', "w")
-            del pref['set_auto_referral'][str(short)]
-            pref = json.dumps(pref, indent= 4)
-            JSON.write(pref)
-            JSON.close()
-            await ctx.send(f"successfully removed short: {short}")
-        else:
-            await ctx.send("specify short name")
-
-    #print subcommand
-    elif mode.lower() == "print":
-        try:
-            JSON = open(f'{ctx.author.id}.json', "r")
-            pref = json.load(JSON)
-            JSON.close()
-            await ctx.send(f'short {short} is mapped to ship ' + str(pref['set_auto_referral'][str(short)]))
-        except:
-            await ctx.send(f"{short} not found.")
-
-    else:
-        await ctx.send("Operation not found, please run :short help for help on the command.")
-    """
 
 
 @bot.command(name="hitlist")
@@ -320,60 +208,25 @@ async def hitlist(ctx, player = "", *, arg):
     elif mode == "viewall":
         string = ""
         for key, value in pref.items():
-            string += f'{str(key)} is in the hitlist for {str(value)}'
+            string += f'{str(key)} is in the hitlist for {str(value)} \n'
         if string != "":
             await ctx.send(string)
         else:
             await ctx.send("hitlist is empty.")
 
 
-    """
-    if mode != "":
-        if mode == "add":
-            try:
-                JSON = open("hitlist.json", "r")
-                pref = json.load(JSON)
-                JSON.close()
-                JSON = open("hitlist.json", "w")
-                pref[str(player)] = str(reason)
-                pref = json.dumps(pref, indent=4)
-                JSON.write(pref)
-                JSON.close()
-                await ctx.send(f"Successfully added player {player} to hitlist for {reason}")
-            except Exception as e:
-                await ctx.send(str(e))
-
-        if mode == "view":
-            try:
-                JSON = open("hitlist.json", "r")
-                pref = json.load(JSON)
-                await ctx.send(str(pref[player]))
-            except KeyError:
-                await ctx.send("Player not found")
-
-        if mode == "viewall":
-            try:
-                JSON = open("hitlist.json", "r")
-                pref = json.load(JSON)
-                await ctx.send(str(pref))
-
-            except Exception as e:
-                await ctx.send(str(e))
-    """
-
-
 @bot.command(name="range")
-async def Range(ctx, arg):
+async def Range(ctx, *, arg):
     await shipCommand(ctx, arg, "range")
 
 
 @bot.command(name="info")
-async def info(ctx, arg):
+async def info(ctx, *, arg):
     await shipCommand(ctx, arg, "info")
 
 
 @bot.command(name="turret")
-async def turret(ctx, arg):
+async def turret(ctx, *, arg):
     await shipCommand(ctx, arg, "turret")
 
 
@@ -381,27 +234,27 @@ async def turret(ctx, arg):
 async def help(ctx):
     await ctx.send(f"This is version {Version} of Tau Gamma Bot, home brewed by epicbuilder2007, who definitely doesn't have other stuff to do. \n\n"+
                    "There are currently  commands for this bot: \n\n"+
-                   "command :info <string> \n"+
+                   "command !info <string> \n"+
                    "This returns general info of the ship. \n\n"+
-                   "command :turret <string> \n"+
+                   "command !turret <string> \n"+
                    "This returns turret info (dps and name) of the ship. \n\n"+
-                   "command :range <string> \n"+
+                   "command !range <string> \n"+
                    "This returns range info of the ship \n\n"+
-                   "command :short <short> <ship (optional)> <set/remove/print> \n"+
+                   "command !short <short> <ship (optional)> <set/remove/print> \n"+
                    "Use this command to set shortcuts to ships.\n"+
                    "'set' sets the short name you provided to the ship name you provided. Remember that currently, you do need to make sure the ship name is case-correct. \n"+
                    "'remove' removes the short name you provided. \n"+
                    "'print' prints the short name you set before \n\n"+
-                   "command :hitlist <player> <reason (optional)> <add/view/viewall> \n"+
+                   "command !hitlist <player> <reason (optional)> <add/view/viewall> \n"+
                    "Use this command to keep tabs on which players the clan wants to kill. \n"+
                    "'add' adds the player to the list. \n"+
                    "'view' views the reason why the player was added to the hitlist. \n"+
                    "'viewall' views all the players and their reason for their presence in the hitlist \n\n"+
-                   "command :suslist <player> <add/view> \n"+
+                   "command !suslist <player> <add/view> \n"+
                    "Use this command to keep track of suspicious players to avoid."+
                    "'add' adds the player name provided to the list. \n"+
                    "'view' returns the list of players to be careful around. \n\n"+
-                   "command :services <service> <True/False> \n"+
+                   "command !services <service> <True/False> \n"+
                    "Use this command to enable or disable bot features for yourself. \n"+
                    "Current configurable services include (case-sensitive): 'predictive_search' \n\n"+
                    "If you have any feature requests, or bug reports, feel free to contact me (epicbuilder2007#8204) about it, whether via pinging me in General Chat or by DMing me, or even via Whatsapp if you have it."+
@@ -435,24 +288,20 @@ async def services(ctx, service, status = True):
 
 
 @bot.command(name="data")
-async def data(ctx, msg: discord.Message , ship = "", cat = "", string = ""):
-    #check if all args aren't empty DONE
-    #if so, then add the request into a new entry in json DONE
-    #during bot startup, create a subprocess that checks the json file every now and then
-    #if amount of people reacting positively is over 50%, then push change to excel file.
-    #to do this, open the excel file in the subprocess, write to it, then close it back.
-    #should change code to load the excel file when command is invoked, instead of startup.
+async def data(ctx, ship = "", cat = "", string = ""):
     if ship != "" and cat != "" and string != "":
         JSON = open("data.json", "r")
         queue = json.load(JSON)
         JSON.close()
-        queue[str(msg.id)] = (str(ship), str(cat), str(string), 0)
+        message = await ctx.send(f"Successfully proposed change: \n\n ship: {str(ship)} \n Data Category: {str(cat)} \n Value: {str(string)} \n\n Now the council shall decide your fate.")
+        await message.add_reaction(":arrow_up:")
+        await message.add_reaction(":arrow_down:")
         JSON = open("data.json", "w")
+        queue[str(message.id)] = (ship, cat, string)
         queue = json.dumps(queue, indent=4)
         JSON.write(queue)
         JSON.close()
-        message = await ctx.send(f"Successfully proposed change: \n\n ship: {str(ship)} \n Data Category: {str(cat)} \n Value: {str(string)} \n\n Now the council shall decide your fate.")
-        await message.add_reaction(":arrow_up:")
+
     else:
         if ship == "":
             await ctx.send("Please specify ship")
@@ -460,6 +309,7 @@ async def data(ctx, msg: discord.Message , ship = "", cat = "", string = ""):
             await ctx.send("Please specify data category")
         if string == "":
             await ctx.send("Please specify data value")
+
 
 
 @bot.command(name="suslist")
@@ -475,8 +325,8 @@ async def sus(ctx, player = "", mode = "add"):
         JSON.close()
     elif mode.lower() == "view":
         string = []
-        for key in pref.items():
-            string.append(key)
+        for key, value in pref.items():
+            string.append(str(key))
         await ctx.send(f'Players in this list: {", ".join(string)}. Please be careful when around these players, as \
         they are known for ruining the fun.')
 
