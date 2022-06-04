0b222efa5ba3817965d8d88202e4a482513a6b93
epicbuilder2007 <elonlou@gmail.com>
2022-06-04 16:42:00 +0800

    bot now also tries to get data from the wiki

--- discord_bot.py
+++ discord_bot.py
@@ -1,4 +1,5 @@
 import os
+import codecs
 import discord
 from dotenv import load_dotenv
 import pandas
@@ -10,7 +11,7 @@ from numpy import ndarray
 
 Version = 1.8
 
-Galaxylist = pandas.read_excel('Galaxydps.xlsx', index_col = 0)
+Galaxylist = pandas.read_excel('Galaxydps.xlsx', index_col=0)
 
 
 async def writeJSON(ctx, ship, arg):
@@ -21,7 +22,7 @@ async def writeJSON(ctx, ship, arg):
     pref["last_search_ship_type"] = str(Galaxylist['Type'][str(ship)])
     pref["last_search_string"] = arg
     pref["last_search_return_ship"] = ship
-    pref = json.dumps(pref, indent = 4)
+    pref = json.dumps(pref, indent=4)
     JSON.write(pref)
     JSON.close()
 
@@ -29,12 +30,14 @@ async def writeJSON(ctx, ship, arg):
 async def concTurret(ctx, ship):
     string = ""
     for i in range(2):
-        if str(Galaxylist[f'Spinal{i+1}_type'][str(ship)]) != "None":
-            string = string + f"Spinal {i+1}: " + str(Galaxylist[f'Spinal{i+1}_type'][str(ship)]) + " x" + str(int(Galaxylist[f'Barrels{i+1}'][str(ship)])) + "\n"
+        if str(Galaxylist[f'Spinal{i + 1}_type'][str(ship)]) != "None":
+            string = string + f"Spinal {i + 1}: " + str(Galaxylist[f'Spinal{i + 1}_type'][str(ship)]) + " x" + str(
+                int(Galaxylist[f'Barrels{i + 1}'][str(ship)])) + "\n"
 
     for i in range(10):
-        if str(Galaxylist[f'Turret{i+1}_type'][str(ship)]) != "None":
-            string = string + f'Turret {i+1}: ' + str(Galaxylist[f'Turret{i+1}_type'][str(ship)]) + " x" + str(int(Galaxylist[f'Turret{i+1}_amount'][str(ship)])) + "\n"
+        if str(Galaxylist[f'Turret{i + 1}_type'][str(ship)]) != "None":
+            string = string + f'Turret {i + 1}: ' + str(Galaxylist[f'Turret{i + 1}_type'][str(ship)]) + " x" + str(
+                int(Galaxylist[f'Turret{i + 1}_amount'][str(ship)])) + "\n"
 
     return str(string)
 
@@ -42,32 +45,64 @@ async def concTurret(ctx, ship):
 async def outputResult(ctx, ship, mode):
     try:
         if mode == "info":
-            await ctx.send(
-                "Ship: " + str(ship) +"\n"
-                "Ship Category: " + str(Galaxylist['Type'][str(ship)]) + "\n"
-                "Ship Status: " + str(Galaxylist['isLimited'][str(ship)]) + "\n"
-                "Average DPS: " + str(Galaxylist['AvDPS'][str(ship)]) + "\n"
-                "Shield DPS: " + str(Galaxylist['ShieldDPS'][str(ship)]) +"\n"
-                "Hull DPS: " + str(Galaxylist['HullDPS'][str(ship)]) +"\n"
-                "Turret DPS: " + str(Galaxylist['TurretDPS'][str(ship)]) +"\n"
-                "Spinal DPS: " + str(Galaxylist['SpinalDPS'][str(ship)]) +"\n"
-                "Minimum Range: " + str(Galaxylist['minRange'][str(ship)]) +"\n"
-                "Maximum Range: " + str(Galaxylist['maxRange'][str(ship)]))
+            string = \
+                "Ship: " + str(ship) + "\n" +\
+                "Ship Category: " + str(Galaxylist['Type'][str(ship)]) + "\n" +\
+                "Ship Status: " + str(Galaxylist['isLimited'][str(ship)]) + "\n" +\
+                "Average DPS: " + str(Galaxylist['AvDPS'][str(ship)]) + "\n" +\
+                "Shield DPS: " + str(Galaxylist['ShieldDPS'][str(ship)]) + "\n" +\
+                "Hull DPS: " + str(Galaxylist['HullDPS'][str(ship)]) + "\n" +\
+                "Turret DPS: " + str(Galaxylist['TurretDPS'][str(ship)]) + "\n" +\
+                "Spinal DPS: " + str(Galaxylist['SpinalDPS'][str(ship)]) + "\n" +\
+                "Minimum Range: " + str(Galaxylist['minRange'][str(ship)]) + "\n" +\
+                "Maximum Range: " + str(Galaxylist['maxRange'][str(ship)]) + "\n"
+
+            global data
+            try:
+                data = codecs.open(ship, 'r')
+            except FileNotFoundError:
+                os.system(f"wget https://robloxgalaxy.wiki/wiki/{ship}")
+                data = codecs.open(ship, 'r')
+
+            list = str(data.read()).split("\n")
+            info = []
+            for i in range(len(list)):
+                if list[i].find('<td class="pi-horizontal-group-item pi-data-value pi-font pi-border-color pi-item-spacing" data-source=') >= 0:
+                    info.append(list[i])
+
+            for i in range(len(info)):
+                info[i] = info[i].replace(
+                    '<td class="pi-horizontal-group-item pi-data-value pi-font pi-border-color pi-item-spacing" data-source=',
+                    '')
+                info[i] = info[i].replace('<p>', '')
+                info[i] = info[i].replace('</p>', '')
+                info[i] = info[i].replace('</td>', '')
+                info[i] = info[i].replace('>', ': ')
+                info[i] = info[i].replace('"', '')
+                info[i] = info[i].replace('credit', 'manufacturing fee')
+                info[i] = info[i].replace('_', ' ')
+                info[i] = info[i].title()
+                info[i] += "\n"
+                string += info[i]
+
+            await ctx.send(string)
+
+
         elif mode == "turret":
             string = await concTurret(ctx, ship)
             await ctx.send(
-                "Ship: " + str(ship) +"\n"
+                "Ship: " + str(ship) + "\n"
                 "Average DPS: " + str(Galaxylist['AvDPS'][str(ship)]) + "\n"
-                "Shield DPS: " + str(Galaxylist['ShieldDPS'][str(ship)]) +"\n"
-                "Hull DPS: " + str(Galaxylist['HullDPS'][str(ship)]) +"\n"
-                "Turret DPS: " + str(Galaxylist['TurretDPS'][str(ship)]) +"\n"
-                "Spinal DPS: " + str(Galaxylist['SpinalDPS'][str(ship)]) +"\n" +
+                "Shield DPS: " + str(Galaxylist['ShieldDPS'][str(ship)]) + "\n"
+                "Hull DPS: " + str(Galaxylist['HullDPS'][str(ship)]) + "\n"
+                "Turret DPS: " + str(Galaxylist['TurretDPS'][str(ship)]) + "\n"
+                "Spinal DPS: " + str(Galaxylist['SpinalDPS'][str(ship)]) + "\n" +
                 string
-                )
+            )
         elif mode == "range":
             await ctx.send(
                 "Ship: " + str(ship) + "\n"
-                "Minimum Range: " + str(Galaxylist['minRange'][str(ship)]) +"\n"
+                "Minimum Range: " + str(Galaxylist['minRange'][str(ship)]) + "\n"
                 "Maximum Range: " + str(Galaxylist['maxRange'][str(ship)]))
     except:
         await ctx.send("Please check if you included mode argument. the command is :ship <ship> <info/turret>")
@@ -82,7 +117,7 @@ async def newJSON(ctx):
             'last_search_return_ship': "",
             'set_auto_referral': {}
         }
-        pref = json.dumps(pref, indent = 4)
+        pref = json.dumps(pref, indent=4)
         JSON.write(pref)
         JSON.close()
 
@@ -125,21 +160,6 @@ async def shipCommand(ctx, arg, mode):
 load_dotenv()
 TOKEN = ""
 bot = commands.Bot(command_prefix="!")
-
-
-#Before we start the bot, we need to start an extra thread for checking reactions from !data.
-def checkReaction(ctx):
-    while True:
-        JSON = open("data.json", "r")
-        messages = json.load(JSON)
-        JSON.close()
-        JSON = open("data.json", "w")
-        for key, value in messages.items():
-            channel = bot.get_channel(int(value[0]))
-            msg = channel.fetch_messages(int(key))
-            posreaction = get(msg.reactions, emoji="⬆")
-            negreaction = get(msg.reactions, emoji="⬇")
-           # if posreaction - negreaction >= 3:
 
 
 @bot.command(name='short')
@@ -211,20 +231,20 @@ async def hitlist(ctx, *, arg):
         args.remove(player)
         if player != "":
             pref[player] = " ".join(args)
-            pref = json.dumps(pref, indent = 4)
+            pref = json.dumps(pref, indent=4)
             JSON.write(pref)
             JSON.close()
             await ctx.send(f"Player {player} added to hitlist for reason {' '.join(args)}")
         else:
             await ctx.send("Player not defined.")
     elif mode == "view":
+        player = str(args[0])
         if player != "":
             try:
                 await ctx.send(f'{player} is in the hitlist for {pref[player]}')
             except KeyError:
                 await ctx.send(f'{player} not found. Probably typos.')
-            else:
-                await ctx.send("Player not defined.")
+
     elif mode == "viewall":
         string = ""
         for key, value in pref.items():
@@ -245,6 +265,7 @@ async def hitlist(ctx, *, arg):
         except KeyError:
             await ctx.send(f'unable to find player {player} in list')
 
+
 @bot.command(name="range")
 async def Range(ctx, *, arg):
     await shipCommand(ctx, arg, "range")
@@ -262,37 +283,38 @@ async def turret(ctx, *, arg):
 
 @bot.command(name="Help")
 async def help(ctx):
-    await ctx.send(f"This is version {Version} of Tau Gamma Bot, home brewed by epicbuilder2007, who definitely doesn't have other stuff to do. \n\n"+
-                   "There are currently  commands for this bot: \n\n"+
-                   "command !info <string> \n"+
-                   "This returns general info of the ship. \n\n"+
-                   "command !turret <string> \n"+
-                   "This returns turret info (dps and name) of the ship. \n\n"+
-                   "command !range <string> \n"+
-                   "This returns range info of the ship \n\n"+
-                   "command !short <short> <ship (optional)> <set/remove/print> \n"+
-                   "Use this command to set shortcuts to ships.\n"+
-                   "'set' sets the short name you provided to the ship name you provided. Remember that currently, you do need to make sure the ship name is case-correct. \n"+
-                   "'remove' removes the short name you provided. \n"+
-                   "'print' prints the short name you set before \n\n"+
-                   "command !hitlist <player> <reason (optional)> <add/view/viewall> \n"+
-                   "Use this command to keep tabs on which players the clan wants to kill. \n"+
-                   "'add' adds the player to the list. \n"+
-                   "'view' views the reason why the player was added to the hitlist. \n"+
-                   "'viewall' views all the players and their reason for their presence in the hitlist \n\n"+
-                   "command !suslist <player> <add/view> \n"+
-                   "Use this command to keep track of suspicious players to avoid."+
-                   "'add' adds the player name provided to the list. \n"+
-                   "'view' returns the list of players to be careful around. \n\n"+
-                   "command !services <service> <True/False> \n"+
-                   "Use this command to enable or disable bot features for yourself. \n"+
-                   "Current configurable services include (case-sensitive): 'predictive_search' \n\n"+
-                   "If you have any feature requests, or bug reports, feel free to contact me (epicbuilder2007#8204) about it, whether via pinging me in General Chat or by DMing me, or even via Whatsapp if you have it."+
-                   "Don't waste your time trying to tell me how shit this bot is though, I know that :sad:")
+    await ctx.send(
+        f"This is version {Version} of Tau Gamma Bot, home brewed by epicbuilder2007, who definitely doesn't have other stuff to do. \n\n" +
+        "There are currently  commands for this bot: \n\n" +
+        "command !info <string> \n" +
+        "This returns general info of the ship. \n\n" +
+        "command !turret <string> \n" +
+        "This returns turret info (dps and name) of the ship. \n\n" +
+        "command !range <string> \n" +
+        "This returns range info of the ship \n\n" +
+        "command !short <short> <ship (optional)> <set/remove/print> \n" +
+        "Use this command to set shortcuts to ships.\n" +
+        "'set' sets the short name you provided to the ship name you provided. Remember that currently, you do need to make sure the ship name is case-correct. \n" +
+        "'remove' removes the short name you provided. \n" +
+        "'print' prints the short name you set before \n\n" +
+        "command !hitlist <player> <reason (optional)> <add/view/viewall> \n" +
+        "Use this command to keep tabs on which players the clan wants to kill. \n" +
+        "'add' adds the player to the list. \n" +
+        "'view' views the reason why the player was added to the hitlist. \n" +
+        "'viewall' views all the players and their reason for their presence in the hitlist \n\n" +
+        "command !suslist <player> <add/view> \n" +
+        "Use this command to keep track of suspicious players to avoid." +
+        "'add' adds the player name provided to the list. \n" +
+        "'view' returns the list of players to be careful around. \n\n" +
+        "command !services <service> <True/False> \n" +
+        "Use this command to enable or disable bot features for yourself. \n" +
+        "Current configurable services include (case-sensitive): 'predictive_search' \n\n" +
+        "If you have any feature requests, or bug reports, feel free to contact me (epicbuilder2007#8204) about it, whether via pinging me in General Chat or by DMing me, or even via Whatsapp if you have it." +
+        "Don't waste your time trying to tell me how shit this bot is though, I know that :sad:")
 
 
 @bot.command(name='services')
-async def services(ctx, service, status = True):
+async def services(ctx, service, status=True):
     global JSON
     try:
         JSON = open(f'{ctx.author.id}.json', 'r')
@@ -318,16 +340,17 @@ async def services(ctx, service, status = True):
 
 
 @bot.command(name="data")
-async def data(ctx, ship = "", cat = "", string = ""):
+async def data(ctx, ship="", cat="", string=""):
     if ship != "" and cat != "" and string != "":
         JSON = open("data.json", "r")
         queue = json.load(JSON)
         JSON.close()
-        message = await ctx.send(f"Successfully proposed change: \n\n ship: {str(ship)} \n Data Category: {str(cat)} \n Value: {str(string)} \n\n Now the council shall decide your fate.")
+        message = await ctx.send(
+            f"Successfully proposed change: \n\n ship: {str(ship)} \n Data Category: {str(cat)} \n Value: {str(string)} \n\n Now the council shall decide your fate.")
         await message.add_reaction("⬆")
         await message.add_reaction("⬇")
         JSON = open("data.json", "w")
-        queue[str(message.id)] = (message.channel.id , ship, cat, string)
+        queue[str(message.id)] = (message.channel.id, ship, cat, string)
         queue = json.dumps(queue, indent=4)
         JSON.write(queue)
         JSON.close()
@@ -339,7 +362,6 @@ async def data(ctx, ship = "", cat = "", string = ""):
             await ctx.send("Please specify data category")
         if string == "":
             await ctx.send("Please specify data value")
-
 
 
 @bot.command(name="suslist")
@@ -357,7 +379,7 @@ async def sus(ctx, *, arg):
         elif "remove" in args:
             mode = "remove"
         args.remove(mode)
-    if mode != view:
+    if mode != "view":
         try:
             player = args[0]
             if mode == "add":
@@ -366,17 +388,11 @@ async def sus(ctx, *, arg):
                 pref = json.dumps(pref, indent=4)
                 JSON.write(pref)
                 JSON.close()
-            elif mode == "view":
-                string = []
-                for key, value in pref.items():
-                    string.append(str(key))
-                await ctx.send(f'Players in this list: {", ".join(string)}. Please be careful when around these players, as \
-                they are known for ruining the fun.')
             elif mode == "remove":
                 try:
                     JSON = open("suslist.json", "w")
                     del pref[player]
-                    pref = json.dumps(pref, indent = 4)
+                    pref = json.dumps(pref, indent=4)
                     JSON.write(pref)
                     JSON.close()
                     await ctx.send(f"Player {player} removed.")
@@ -390,7 +406,6 @@ async def sus(ctx, *, arg):
             string.append(str(key))
         await ctx.send(f'Players in this list: {", ".join(string)}. Please be careful when around these players, as \
         they are known for ruining the fun.')
-
 
 
 bot.run(TOKEN)
